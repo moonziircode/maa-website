4363206faf323766bf59e2b6aec54ede57a6dad1
moonziircode <aqsa.mflhn@gmail.com>
2026-06-03 23:26:49 +0700

    Hardcode Firebase config

--- firebase-config.js
+++ firebase-config.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_DATABASE_URL,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID
+  apiKey: "AIzaSyAXTecT6ngm5njThbIzu4bivqq_zQiacAI",
+  authDomain: "tab-maa-prod.firebaseapp.com",
+  databaseURL: "https://tab-maa-prod.firebaseio.com",
+  projectId: "tab-maa-prod",
+  storageBucket: "tab-maa-prod.appspot.com",
+  messagingSenderId: "426734759783",
+  appId: "1:426734759783:web:xxxxxxxxxxxxxx"
 };
 
 // Initialize Firebase
